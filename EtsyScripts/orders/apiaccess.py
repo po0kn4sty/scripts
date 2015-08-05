f49d20a0d1b26ba1592c1af1fa96c111e2212608
Eric Muller <pooknasty@Erics-MacBook-Air.local>
2015-08-04 23:16:30 -0700

    findAllShopReceipts() added to apiaccess

--- EtsyScripts/orders/apiaccess.py
+++ EtsyScripts/orders/apiaccess.py
@@ -5,7 +5,7 @@ from requests_oauthlib import OAuth1Session
 baseRequestURI = 'https://openapi.etsy.com/v2/'
 
 with open('../resources/EtsyAPI.ck', 'r') as f:
-    exec f.read() in globals()
+    exec(f.read(), globals())
 
 oauth = OAuth1Session(client_key, client_secret=client_secret,\
         resource_owner_key=oauth_token, resource_owner_secret=oauth_token_secret)
@@ -14,22 +14,23 @@ def findAllShopTransactions(shop, limit=25, offset=0):
     request_URI = baseRequestURI + """shops/%s/transactions?limit=%s&offset=%s""" % (shop, str(limit), str(offset))
     return oauth.get(request_URI) 
 
-
-finaldata = []
-#x = findAllShopTransactions("caresspress", 100)
-#data = x.json()
-#finaldata += data['results']
+def findAllShopReceipts(shop, limit=25, offset=0, was_shipped=None):
+    request_URI = baseRequestURI
+    if was_shipped is not None:
+        request_URI += """shops/%s/receipts?limit=%s&offset=%swas_shipped=%s"""\
+                % (shop, str(limit), str(offset), str(was_shipped).lower())
+    else:
+        request_URI += """shops/%s/receipts?limit=%s&offset=%s"""\
+                % (shop, str(limit), str(offset))
+    return oauth.get(request_URI)
 
 total = 4049
 oset = 0
-while oset < total:
-    x = findAllShopTransactions('caresspress', 100, oset)
-    data = x.json()
-    finaldata += data['results']
+finaldata = []
+while (oset < total):
+    response = findAllShopTransactions('caresspress', 100, oset)
+    jsondata = response.json()
+    finaldata += jsondata['results']
     oset += 100
-
-f = open('testout.txt', 'w')
-for item in finaldata:
-    f.write(str(item['variations']) + '\n')
-
-f.close()
+oset = 0
+x = findAllShopReceipts('caresspress')
